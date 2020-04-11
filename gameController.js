4e094b0176c0ccb6ac00b94a82fe670927799509
bhatit2 <tanviips786@gmail.com>
2020-04-11 19:14:29 +0530

    Update gameController.js

--- gameController.js
+++ gameController.js
@@ -23,3 +23,34 @@ function cowsAndBulls(guess, actual) {
 return res;
 }
 
+function generateNumForGame() {
+    let n;
+    let temp = [];
+    while(temp.length !== 3) {
+        temp = [];
+        n = Math.floor(Math.random()*(999-100+1)+100);
+        let arr = n.toString().split('');
+        arr.forEach(val => {
+            if(temp.indexOf(val) === -1) temp.push(val);
+        });
+    }
+    return n;
+}
+
+let gamePlay = (function() {
+    let count = 0;
+    let actual = generateNumForGame();
+    let res = { cows : 0, bulls : 0};
+    return function* (guess) {
+        res = cowsAndBulls(guess, actual);
+        while(res.bulls !== 3) {
+            count++;
+            yield res;
+        }
+        return `You won in ${count} attempts!`;
+    }
+})();
+
+function playCowsAndBulls(guess) {
+    return gamePlay(guess).next().value;
+}
